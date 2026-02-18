f6a35420c49fe20239d32159c70f6fc2e66268f0
steven <steven.lavaure@hotmail.Fr>
2026-02-18 13:26:53 +0100

    Ajoutez des fichiers projet.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+//écrire
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("test");
+
+Console.WriteLine(Test());
+
+
+
+//lire
+//Console.ReadLine();
+
+ string Test()
+{
+   return "Test";
+}
+
+Console.WriteLine(Test());
+
+
+void calculate(string calcule)
+{
+    Console.WriteLine("Rentre ton calcul !");
+    string response = Console.ReadLine();
+
+    char[] ops = ['+', '-', '*', '%'];
+    int i = response.IndexOfAny(ops);
+
+    int number1 = int.Parse(response.Substring(0, i));
+    int number2 = int.Parse(response.Substring(i + 1));
+    char operatorCalcul = response[i];
+
+    int result = operatorCalcul switch
+    {
+        '+' => number1 + number2,
+        '-' => number1 - number2,
+        '' => number1 * number2,
+        '%' => number1 % number2
+    };
+
+    Console.WriteLine($"result : {result}");
+}
+
+calculate("3+5");
